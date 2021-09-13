131967e5546d3fdee66be11fb76721ab7f33d6d3
Loki701 <figueredoj189@gmail.com>
2021-09-12 23:15:55 -0400

    Added the skill section of the page and improved the UI for the navigation bar

--- frontend/src/components/Sections/Skills.js
+++ frontend/src/components/Sections/Skills.js
@@ -1,9 +1,39 @@
+import './Skills.css'
+
 export default function Skills() {
+
+    const skillsArray = [
+        {'title': 'HTML & CSS', 'percent': '80%'},,
+        {'title': 'JavaScript', 'percent': '70%'},
+        {'title': 'Python', 'percent': '65%'},
+        {'title': 'C++', 'percent': '65%'},
+        {'title': 'React', 'percent': '50%'},
+        {'title': 'MongoDB', 'percent': '50%'},
+        {'title': 'FastAPI', 'percent': '50%'},
+    ]
+
+    const listSkills = skillsArray.map((skill) =>{
+        return <div className ="skills-row">
+            <h1 className="skills-bar-title">{skill.title}</h1>
+            <div className="skills-progress-bar">
+            <div className = "skills-progress" style={{height: '1rem',width: `${skill.percent}`}}/>
+            <p style={{fontSize: '1.2rem'}}>{skill.percent}</p>
+            </div>
+        </div>
+    })
   return (
     <>
       <a name="Skills" />
       <div className="skills">
-        <p>Skills!</p>
+        <div className="skills-container">
+            <div className='skills-title'>
+                <h1> MY KNOWLEDGE LEVEL IN SOFTWARE</h1>
+            </div>
+
+            <div className="skills-table">
+                {listSkills}
+            </div>
+        </div>
       </div>
     </>
   );
